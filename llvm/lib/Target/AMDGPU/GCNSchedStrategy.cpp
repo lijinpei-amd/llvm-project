ae1300573105d6af5c6fac1c3e0e22a6fc1246b3
Li Jinpei <jinpli@amd.com>
2026-02-07 18:53:49 +0800

    disable un-accurate scheduling redo

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
@@ -2131,6 +2131,7 @@ bool MemoryClauseInitialScheduleStage::shouldRevertScheduling(
 }
 
 bool GCNSchedStage::mayCauseSpilling(unsigned WavesAfter) {
+  return false;
   if (WavesAfter <= MFI.getMinWavesPerEU() && isRegionWithExcessRP() &&
       !PressureAfter.less(MF, PressureBefore)) {
     LLVM_DEBUG(dbgs() << "New pressure will result in more spilling.\n");
