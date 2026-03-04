6a80820335454058b134981e112cc63e20206d43
Li Jinpei <jinpli@amd.com>
2026-03-04 15:07:39 +0800

    [NFC][MISched][AMDGPU] Expose tracePick to be used in AMDGPU backend
    - Fix wrong IsTopNode for `PostGenericScheduler::pickNode`.
    - Add tracePick to pre-ra pickOnlyChoice.
    - Applies eaff28c9 to GCNSchedStrategy
    - Fix in-accurate GCNSchedStrategy::tryCandidate reason.

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.h
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.h
@@ -173,7 +173,7 @@ public:
 
 /// The goal of this scheduling strategy is to maximize kernel occupancy (i.e.
 /// maximum number of waves per simd).
-class GCNMaxOccupancySchedStrategy final : public GCNSchedStrategy {
+class GCNMaxOccupancySchedStrategy : public GCNSchedStrategy {
 public:
   GCNMaxOccupancySchedStrategy(const MachineSchedContext *C,
                                bool IsLegacyScheduler = false);
@@ -199,6 +199,54 @@ protected:
 
 public:
   GCNMaxMemoryClauseSchedStrategy(const MachineSchedContext *C);
+};
+
+class GCNPreRACriticalResource final : public GCNMaxOccupancySchedStrategy {
+protected:
+  bool TrackRemCriticalRes;
+
+  unsigned RemCriticalRes;
+
+  bool tryCandidate(SchedCandidate &Cand, SchedCandidate &TryCand,
+                    SchedBoundary *Zone) const override;
+
+  void updateRemainderCriticalRes();
+
+  void initialize(ScheduleDAGMI *DAG) override;
+
+  void schedNode(SUnit *SU, bool IsTopNode) override;
+
+public:
+  GCNPreRACriticalResource(const MachineSchedContext *C,
+                           bool IsLegacyScheduler = false)
+      : GCNMaxOccupancySchedStrategy(C, IsLegacyScheduler) {}
+
+  void setTrackRemainderCriticalRes(const GCNSubtarget &ST, bool B) {
+    TrackRemCriticalRes = B && ST.hasGFX940Insts();
+  }
+};
+
+class GCNPostRACriticalResource final : public PostGenericScheduler {
+protected:
+  bool TrackRemCriticalRes;
+
+  unsigned RemCriticalRes;
+
+  bool tryCandidate(SchedCandidate &Cand, SchedCandidate &TryCand) override;
+
+  void updateRemainderCriticalRes();
+
+  void initialize(ScheduleDAGMI *Dag) override;
+
+  void schedNode(SUnit *SU, bool IsTopNode) override;
+
+public:
+  GCNPostRACriticalResource(const MachineSchedContext *C)
+      : PostGenericScheduler(C) {}
+
+  void setTrackRemainderCriticalRes(const GCNSubtarget &ST, bool B) {
+    TrackRemCriticalRes = B && ST.hasGFX940Insts();
+  }
 };
 
 class ScheduleMetrics {
@@ -272,6 +320,8 @@ class GCNScheduleDAGMILive final : public ScheduleDAGMILive {
 
   SIMachineFunctionInfo &MFI;
 
+  unsigned CurrentRegionIdx = ~0U;
+
   // Occupancy target at the beginning of function scheduling cycle.
   unsigned StartingOccupancy;
 
@@ -339,6 +389,8 @@ public:
   void schedule() override;
 
   void finalizeSchedule() override;
+
+  bool hasIGLPInstrs() const { return RegionsWithIGLPInstrs[CurrentRegionIdx]; }
 };
 
 // GCNSchedStrategy applies multiple scheduling stages to a function.
@@ -816,6 +868,8 @@ public:
   GCNPostScheduleDAGMILive(MachineSchedContext *C,
                            std::unique_ptr<MachineSchedStrategy> S,
                            bool RemoveKillFlags);
+
+  bool hasIGLPInstrs() const { return HasIGLPInstrs; }
 };
 
 } // End namespace llvm
