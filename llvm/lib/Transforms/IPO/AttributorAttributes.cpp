b499bcb83467b72569fd925a15eadf7cba3802ca
Li Jinpei <jinpli@amd.com>
2026-06-24 00:54:12 +0800

    [Attributor] Guard alignment manifests for pointer vectors

--- llvm/lib/Transforms/IPO/AttributorAttributes.cpp
+++ llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -5340,7 +5340,9 @@ struct AAAlignImpl : AAAlign {
     ChangeStatus Changed = AAAlign::manifest(A);
 
     Align InheritAlign =
-        getAssociatedValue().getPointerAlignment(A.getDataLayout());
+        getAssociatedValue().getType()->isPointerTy()
+            ? getAssociatedValue().getPointerAlignment(A.getDataLayout())
+            : Align(1);
     if (InheritAlign >= getAssumedAlign())
       return InstrChanged;
     return Changed | InstrChanged;
@@ -5492,7 +5494,9 @@ struct AAAlignCallSiteArgument final : AAAlignFloating {
         return ChangeStatus::UNCHANGED;
     ChangeStatus Changed = AAAlignImpl::manifest(A);
     Align InheritAlign =
-        getAssociatedValue().getPointerAlignment(A.getDataLayout());
+        getAssociatedValue().getType()->isPointerTy()
+            ? getAssociatedValue().getPointerAlignment(A.getDataLayout())
+            : Align(1);
     if (InheritAlign >= getAssumedAlign())
       Changed = ChangeStatus::UNCHANGED;
     return Changed;
